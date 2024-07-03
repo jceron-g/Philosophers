ceec75e4217c5e002eeae7bc4e0a9640b21e0546
Jose Antonio Ceron <jaceron95@gmail.com>
2024-07-03 20:23:28 +0200

    Starting philo

--- src/philo.h
+++ src/philo.h
@@ -6,15 +6,26 @@
 /*   By: jceron-g <jceron-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 13:02:31 by jceron-g          #+#    #+#             */
-/*   Updated: 2024/06/11 13:04:51 by jceron-g         ###   ########.fr       */
+/*   Updated: 2024/07/03 20:22:39 by jceron-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PHILO_H
+# define PHILO_H
 
 # include "../lib/libft/libft.h"
 
+typedef struct s_philo
+{
+	int				id;
+	long			meals_eaten;
+	long			last_meal;
+	pthread_mutex_t *r_fork;
+	pthread_mutex_t *l_fork;
+	
+	
+	
+}				t_philo
 
 
 #endif
